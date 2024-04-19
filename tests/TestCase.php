3c8a522fac4438cde2159679adc7c63aa1af728a
Tally Schmeits <tally@schmeits.com>
2024-04-19 14:14:32 +0200

    Test runners fixed

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,11 +3,12 @@
 namespace Schmeits\Pulse\DatabaseTableInfo\Tests;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    //use DatabaseTransactions;
+    use LazilyRefreshDatabase;
     use WithWorkbench;
 }
