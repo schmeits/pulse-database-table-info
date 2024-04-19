927ab3a5023b2cf6322675fd1ca9f695acf8dd00
schmeits <schmeits@users.noreply.github.com>
2024-04-19 12:14:56 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Schmeits\Pulse\DatabaseTableInfo\Tests;
 
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
