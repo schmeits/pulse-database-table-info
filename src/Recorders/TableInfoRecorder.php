2a621dea60c936bb6b8b43188fe0ce8bfe38478c
Sun HaiXin <nirvana488939@gmail.com>
2024-06-14 14:58:12 +0800

    Fix bug in json_decode by correcting parameter usage

--- src/Recorders/TableInfoRecorder.php
+++ src/Recorders/TableInfoRecorder.php
@@ -51,7 +51,7 @@ class TableInfoRecorder
                 return empty($obj);
             })->toJson();
 
-            json_decode($results, options: JSON_THROW_ON_ERROR);
+            json_decode($results, false, 512, JSON_THROW_ON_ERROR);
 
             $this->pulse->set('database-tables-info', 'result', $results);
         });
