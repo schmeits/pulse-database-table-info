1170ebc267585e8526f8233ad9d240887560d59a
schmeits <schmeits@users.noreply.github.com>
2024-06-20 08:14:09 +0000

    Fix styling

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -25,8 +25,5 @@ class WorkbenchServiceProvider extends ServiceProvider
     /**
      * Bootstrap services.
      */
-    public function boot(): void
-    {
-
-    }
+    public function boot(): void {}
 }
